de643d269cac5a5d55408da3498b063e156b474e
Paulo07Reis <pmarcena2004@gmail.com>
2025-11-08 20:48:16 -0300

    feat: Doing the first class

--- modulo_01_fundamentos_do_java/aula01/Sintaxe.java
+++ modulo_01_fundamentos_do_java/aula01/Sintaxe.java
@@ -1,6 +1,6 @@
 package modulo_01_fundamentos_do_java.aula01;
 
-public class sintaxe_e_semantica {
+public class Sintaxe {
     public static void main(String[] args) {
         Conta conta = new Conta(100.00);
 
