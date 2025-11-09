8a68b83a6c5231bc376e7dfce1c1355ca74494c3
Paulo07Reis <pmarcena2004@gmail.com>
2025-11-09 16:17:29 -0300

    feat: Métodos e suas regras

---
+++ modulo_01_fundamentos_do_java/aula06/Metodos.java
@@ -0,0 +1,13 @@
+package modulo_01_fundamentos_do_java.aula06;
+
+public class Metodos {
+
+    //Iniciar com letra minuscula
+    public void findById(String id){}
+
+    //Manter os métodos com apenas uma função
+    public void somarImprimir(){} //Evite
+
+
+
+}
