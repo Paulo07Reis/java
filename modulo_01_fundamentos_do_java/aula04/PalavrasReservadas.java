5769edf5ab4d521cb76702a703fbec2a2ae1a7d4
Paulo07Reis <pmarcena2004@gmail.com>
2025-11-09 15:46:49 -0300

    feat: Palavras reservadas

---
+++ modulo_01_fundamentos_do_java/aula04/PalavrasReservadas.java
@@ -0,0 +1,16 @@
+package modulo_01_fundamentos_do_java.aula04;
+
+public class PalavrasReservadas {
+    public static void main(String[] args) {
+        //byte
+        //short
+        //int
+        //long
+
+        //float
+        //double
+
+        //char
+        //boolean
+    }
+}
