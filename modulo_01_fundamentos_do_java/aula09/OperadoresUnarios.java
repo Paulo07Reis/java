6323f007d580de98364c570dab3182b06b1fec2b
Paulo07Reis <pmarcena2004@gmail.com>
2025-11-11 00:47:48 -0300

    feat: Operadores unarios

---
+++ modulo_01_fundamentos_do_java/aula09/OperadoresUnarios.java
@@ -0,0 +1,17 @@
+package modulo_01_fundamentos_do_java.aula09;
+
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(- n);
+        System.out.println(n);
+        n++;
+        System.out.println(n);
+        System.out.println(n++);
+        System.out.println(n);
+
+        boolean falsa = false;
+        System.out.println(!falsa);
+        System.out.println(falsa);
+    }
+}
