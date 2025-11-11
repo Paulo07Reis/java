d480560a709947f8f5f36b46c36b4a33ed7068e6
Paulo07Reis <pmarcena2004@gmail.com>
2025-11-11 00:40:15 -0300

    feat: Operadores relacionais

---
+++ modulo_01_fundamentos_do_java/aula08/OperadoresRelacionais.java
@@ -0,0 +1,20 @@
+package modulo_01_fundamentos_do_java.aula08;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 2;
+
+        if(n1 > n2){
+            System.out.println("N1 > N2");
+        }
+
+        if (n1 < n2){
+            System.out.println("N1 < N2");
+        }
+
+        if (n1 != n2){
+            System.out.println("N1 != N2");
+        }
+    }
+}
