80b9c494d97cca965f64d1aa59ad386f09529db6
Paulo07Reis <pmarcena2004@gmail.com>
2025-11-11 22:29:20 -0300

    feat: Comentarios

---
+++ modulo_01_fundamentos_do_java/aula11/Comentarios.java
@@ -0,0 +1,16 @@
+package modulo_01_fundamentos_do_java.aula11;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        //Isto é um comentário
+        int n1 = 1;
+        int n2 = 2;
+
+        /*
+        Agora
+        some
+        n1 e n2
+         */
+        System.out.println(n1 + n2);
+    }
+}
